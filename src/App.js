cd76bba98bb9e0337f3495cada158c5f8bbd810f
Sureshkumar <sureshkumar@navadhiti.com>
2025-09-17 19:32:45 +0530

    uncommit the code in the app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "@mui/material";
 import { useNavigate } from "../node_modules/react-router-dom/dist/index";
 import { StyledEngineProvider } from "@mui/material/styles";
@@ -10,7 +10,6 @@ import axios from "axios";
 
 const App = () => {
   const navigate = useNavigate();
-  const ranonce = useRef(false);
 
   useEffect(() => {
     const handleBeforeUnload = (event) => {
@@ -28,48 +27,48 @@ const App = () => {
     };
   }, []);
 
-  // axios.interceptors.response.use(
-  //   (response) => response,
-  //   (error) => {
-  //     if (
-  //       error.response &&
-  //       (error.response.status === 401 || error.response.status === 400)
-  //     ) {
-  //       const errorMessage = error?.response?.data?.message
-  //         ?.trim()
-  //         ?.toLowerCase();
-  //       if (
-  //         errorMessage?.includes("unauthorized") ||
-  //         errorMessage?.includes("token") ||
-  //         errorMessage?.includes("logged")
-  //       ) {
-  //         if (
-  //           localStorage.getItem("contentSessionId") &&
-  //           process.env.REACT_APP_IS_APP_IFRAME === "true"
-  //         ) {
-  //           window.parent.postMessage(
-  //             {
-  //               message: "Logged out!",
-  //             },
-  //             window?.location?.ancestorOrigins?.[0] ||
-  //               window.parent.location.origin
-  //           );
-  //           console.log("if logout!");
-  //           localStorage.clear();
-  //           sessionStorage.clear();
-  //           window.location.reload();
-  //         } else {
-  //           console.log("else logout!");
-  //           localStorage.clear();
-  //           sessionStorage.clear();
-  //           navigate("/login");
-  //           window.location.reload();
-  //         }
-  //       }
-  //     }
-  //     return Promise.reject(error);
-  //   }
-  // );
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (
+        error.response &&
+        (error.response.status === 401 || error.response.status === 400)
+      ) {
+        const errorMessage = error?.response?.data?.message
+          ?.trim()
+          ?.toLowerCase();
+        if (
+          errorMessage?.includes("unauthorized") ||
+          errorMessage?.includes("token") ||
+          errorMessage?.includes("logged")
+        ) {
+          if (
+            localStorage.getItem("contentSessionId") &&
+            process.env.REACT_APP_IS_APP_IFRAME === "true"
+          ) {
+            window.parent.postMessage(
+              {
+                message: "Logged out!",
+              },
+              window?.location?.ancestorOrigins?.[0] ||
+                window.parent.location.origin
+            );
+            console.log("if logout!");
+            localStorage.clear();
+            sessionStorage.clear();
+            window.location.reload();
+          } else {
+            console.log("else logout!");
+            localStorage.clear();
+            sessionStorage.clear();
+            navigate("/login");
+            window.location.reload();
+          }
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return (
     <StyledEngineProvider injectFirst>
