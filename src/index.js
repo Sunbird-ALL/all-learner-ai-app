edd81350a6c14e8c3657c06e020bab6961775812
Gourav More <gourav_m@tekditechnologies.com>
2025-11-06 00:06:38 +0530

    Disable service worker for testing purposes

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ render(
 // Learn more about service workers: https://cra.link/PWA
 
 // TEMPORARY: Set to true to disable service worker for testing
-const DISABLE_SERVICE_WORKER = false;
+const DISABLE_SERVICE_WORKER = true;
 
 if (DISABLE_SERVICE_WORKER) {
   console.log("[Service Worker] Service worker disabled for testing");
