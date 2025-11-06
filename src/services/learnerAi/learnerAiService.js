b5f6866b1b82af2758fcab26c10d72408d4f594c
Ayansaha20 <ayan.saha@goodworklabs.com>
2025-11-06 10:41:36 +0530

    issue #000 feat: Added tags for telugu

--- src/services/learnerAi/learnerAiService.js
+++ src/services/learnerAi/learnerAiService.js
@@ -32,7 +32,7 @@ export const getContent = async (
     )
       url += `&mechanics_id=${options.mechanismId}`;
     if (options.competency) url += `&level_competency=${options.competency}`;
-    if (options.tags && (lang === "en" || lang === "kn"))
+    if (options.tags && (lang === "en" || lang === "kn" || lang === "te"))
       url += `&tags=${options.tags}`;
     if (options.storyMode) url += `&story_mode=${options.storyMode}`;
     if (options.CEFR_level) url += `&CEFR_level=${options.CEFR_level}`;
