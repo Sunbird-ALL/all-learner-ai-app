6ff53d13ae37a1df87c82d880f167dd6206e57d7
Ayansaha20 <ayan.saha@goodworklabs.com>
2025-11-06 18:45:32 +0530

    issue #000 feat: Added new tutorial video for Paragraph flow

--- src/components/Practice/ParagraphFlow.jsx
+++ src/components/Practice/ParagraphFlow.jsx
@@ -1058,7 +1058,7 @@ const ParagraphFlow = ({
               <iframe
                 width="100%"
                 height="340"
-                src={`https://www.youtube.com/embed/sK6YINwogLk?autoplay=1`}
+                src={`https://www.youtube.com/embed/kwJEIqYMKEM?autoplay=1`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
