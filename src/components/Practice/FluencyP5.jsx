1e0915ec6a16409273eebf11ab37d435dac1433c
vishnu vinay <vishnu.vinay@tekditechnologies.com>
2026-02-13 16:12:56 +0530

    IssueId #253672 Fix : M4 -M9  in telemetry showing incorrect level .

--- src/components/Practice/FluencyP5.jsx
+++ src/components/Practice/FluencyP5.jsx
@@ -533,8 +533,13 @@ const FluencyP5 = ({
     currentPracticeStep = progressDatas?.currentPracticeStep;
   }
 
-  const currentLevel = practiceSteps?.[currentPracticeStep]?.titleNew || "L1";
+  let currentLevel = practiceSteps?.[currentPracticeStep]?.titleNew || "L1";
   let apiLevel = `M${level}-${currentLevel}`;
+
+  if (level >= 4 && level <= 9) {
+    currentLevel = practiceSteps?.[currentPracticeStep]?.name;
+    apiLevel = `M${level}-${currentLevel}`;
+  }
 
   const callTelemetry = async () => {
     const sessionId = getLocalData("sessionId");
