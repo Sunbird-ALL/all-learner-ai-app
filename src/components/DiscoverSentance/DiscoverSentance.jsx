78aaaba0c1741be195f81dc62c621317fe3ef5d3
Gourav More <gourav_m@tekditechnologies.com>
2026-03-31 11:49:03 +0530

    Issue #0000 fix: Pass set ids for discovery getset result api

--- src/components/DiscoverSentance/DiscoverSentance.jsx
+++ src/components/DiscoverSentance/DiscoverSentance.jsx
@@ -274,11 +274,14 @@ const SpeakSentenceComponent = () => {
         setCurrentQuestion(currentQuestion + 1);
       } else if (currentQuestion === questions.length - 1) {
         const sub_session_id = getLocalData("sub_session_id");
+        const isThirdDiscoverySet = discoveryHistoryRef.current.length === 2;
         const getSetResultRes = await fetchGetSetResult(
           sub_session_id,
           currentContentType,
           currentCollectionId,
-          totalSyllableCount
+          totalSyllableCount,
+          currentSetTag || undefined,
+          isThirdDiscoverySet
         );
 
         // Call engagement predictor after getsetresult
