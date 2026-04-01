45575cef2a447bb775928d38609eb14b4653da05
Gourav More <gourav_m@tekditechnologies.com>
2026-04-01 17:02:51 +0530

    Issue #0000 fix: session timeout issue fixes

--- src/views/AppContent/AppContent.jsx
+++ src/views/AppContent/AppContent.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, Fragment, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import CustomizedSnackbars from "../../views/Snackbar/CustomSnackbar";
 import LoadingFallback from "../../components/LoadingFallback";
-import { useSessionExpired } from "../../context/SessionExpiredProvider";
 
 const PrivateRoute = (props) => {
   const TOKEN = localStorage.getItem("apiToken");
-  const { notifyMissingToken } = useSessionExpired();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!TOKEN && props.requiresAuth) {
-      notifyMissingToken();
+      navigate("/login");
     }
-  }, [TOKEN, props.requiresAuth, notifyMissingToken]);
+  }, [TOKEN, props.requiresAuth, navigate]);
 
   if (!TOKEN && props.requiresAuth) {
     return null;
