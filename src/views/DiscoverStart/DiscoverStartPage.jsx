4bfad3851aed6285e5e8fd235a6fe301dfcd61b2
palani001 <palanisamy@navadhiti.com>
2026-05-05 19:20:08 +0530

    Fixed audio diagnostic page issue (hotfix).

--- src/views/DiscoverStart/DiscoverStartPage.jsx
+++ src/views/DiscoverStart/DiscoverStartPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import Assesment from '../../components/Assesment/Assesment';
+import React, { useState, useEffect } from "react";
+import Assesment from "../../components/Assesment/Assesment";
+import { AudioDiagnosticModal } from "../../components/AudioDiagnostic";
 
 const DiscoverStart = () => {
-    return <Assesment discoverStart />;
+  const [showDiagnostic, setShowDiagnostic] = useState(false);
+
+  useEffect(() => {
+    if (process.env.REACT_APP_IS_APP_IFRAME !== "true") return;
+    if (!localStorage.getItem("audioDiagnosticShown")) {
+      setShowDiagnostic(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <Assesment discoverStart />
+      <AudioDiagnosticModal
+        show={showDiagnostic}
+        onClose={() => {
+          setShowDiagnostic(false);
+          localStorage.setItem("audioDiagnosticShown", "true");
+        }}
+      />
+    </>
+  );
 };
 
 export default DiscoverStart;
