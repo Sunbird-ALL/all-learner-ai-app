d8ac4e34bb9bd2caa6a501989810478b8911e0eb
palani001 <palanisamy@navadhiti.com>
2026-05-06 12:30:24 +0530

    updated the localstorage store flow.

--- src/views/DiscoverStart/DiscoverStartPage.jsx
+++ src/views/DiscoverStart/DiscoverStartPage.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Assesment from "../../components/Assesment/Assesment";
 import { AudioDiagnosticModal } from "../../components/AudioDiagnostic";
+import { getLocalData, setLocalData } from "../../utils/constants";
 
 const DiscoverStart = () => {
   const [showDiagnostic, setShowDiagnostic] = useState(false);
 
   useEffect(() => {
     if (process.env.REACT_APP_IS_APP_IFRAME !== "true") return;
-    if (!localStorage.getItem("audioDiagnosticShown")) {
+    if (!getLocalData("audioDiagnosticShown")) {
       setShowDiagnostic(true);
     }
   }, []);
@@ -19,7 +20,7 @@ const DiscoverStart = () => {
         show={showDiagnostic}
         onClose={() => {
           setShowDiagnostic(false);
-          localStorage.setItem("audioDiagnosticShown", "true");
+          setLocalData("audioDiagnosticShown", "true");
         }}
       />
     </>
